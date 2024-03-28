42c48268ceb453029306eaca62cfd7f583a78a22
Miragaia <miguelmiragaia2003@gmail.com>
2024-03-28 21:22:03 +0000

    location by firebase

--- findme/lib/main.dart
+++ findme/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'map_screen.dart';
+import 'map_screen2.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
